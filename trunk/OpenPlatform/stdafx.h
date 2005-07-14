05e0d84f362dcb6ca4bcd57ffff29ce370bd2c6f
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2005-07-14 19:31:46 +0000

    This commit was generated by cvs2svn to compensate for changes in r4, which included commits to RCS files with non-trunk default branches.

---
+++ trunk/OpenPlatform/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifdef _WIN32
+	#ifndef _UNICODE
+	#define _UNICODE
+	#endif
+	#ifndef UNICODE
+	#define UNICODE
+	#endif
+#endif
+#ifdef WIN32
+#include <tchar.h>
+#endif
+
+#include <io.h>
+
+// TODO: reference additional headers your program requires here
+
