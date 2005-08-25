33cd6562d58e87877647d727fcda25b56c33aefc
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2005-08-25 04:01:25 +0000

    ChangeLog Windows DLL version number

--- trunk/OpenPlatform/version.h
+++ trunk/OpenPlatform/version.h
@@ -1,4 +1,3 @@
-#define OPSP_VERSION 1,0,0,0
-#define OPSP_VERSION_STRING "1, 0, 0, 0\0"
+#define OPSP_VERSION 1,1,0,0
+#define OPSP_VERSION_STRING "1, 1, 0, 0\0"
 #define OPSP_LIB_NAME "OpenPlatform\0"
-
