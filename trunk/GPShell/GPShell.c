61b5ef8ac5df4cc80b401a27d99b35c6823577d1
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2007-10-03 17:53:50 +0000

    changed license to LGPL

--- trunk/GPShell/GPShell.c
+++ trunk/GPShell/GPShell.c
@@ -1,5 +1,20 @@
+/**
+ *  Copyright (c) 2007, Snit Mo, Karsten Ohme 
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 /* GPShell.c */
-
 #ifdef WIN32
 #include "stdafx.h"
 #else
@@ -715,7 +730,7 @@ static int handleCommands(FILE *fd)
 		break;
 		/* Augusto: added delete_key command support */
 	    } else if (strcmp(token, "delete_key") == 0) {
-			
+
 			rv = handleOptions(&optionStr);
 			if(rv != EXIT_SUCCESS) {
 				goto end;
