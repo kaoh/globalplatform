31917c2bf7badbb44d1d3396ac9471a1ed6079fe
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2010-03-13 13:09:44 +0000

    Fixed used helloworld version

--- helloworldapplet/src/main/java/net/sourceforge/globalplatform/jc/helloworld/HelloWorldApplet.java
+++ helloworldapplet/src/main/java/net/sourceforge/globalplatform/jc/helloworld/HelloWorldApplet.java
@@ -18,34 +18,28 @@ package net.sourceforge.globalplatform.jc.helloworld;
 
 import javacard.framework.APDU;
 import javacard.framework.Applet;
-import javacard.framework.ISO7816;
-import javacard.framework.ISOException;
 import javacard.framework.Util;
 
 /**
+ * Hello World Applet.
  */
-
 public class HelloWorldApplet extends Applet {
-    private byte[] echoBytes;
-    private static final short LENGTH_ECHO_BYTES = 256;
 
-    /**
-     * Only this class's install method should create the applet object.
-     */
-    protected HelloWorldApplet() {
-        echoBytes = new byte[LENGTH_ECHO_BYTES];
+    private final static byte[] HELLO_WORLD = new byte[]{'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
+
+    private HelloWorldApplet() {
         register();
     }
 
     /**
-     * Installs this applet.
+     * Installs the applet.
      *
      * @param bArray
-     *            the array containing installation parameters
+     *            array with installation parameters.
      * @param bOffset
-     *            the starting offset in bArray
+     *            offset into array.
      * @param bLength
-     *            the length in bytes of the parameter data in bArray
+     *            the length of the parameters.
      */
     public static void install(byte[] bArray, short bOffset, byte bLength) {
         new HelloWorldApplet();
@@ -54,73 +48,13 @@ public class HelloWorldApplet extends Applet {
     /**
      * Processes an incoming APDU.
      *
-     * @see APDU
      * @param apdu
-     *            the incoming APDU
-     * @exception ISOException
-     *                with the response bytes per ISO 7816-4
+     *            the APDU.
      */
     public void process(APDU apdu) {
         byte buffer[] = apdu.getBuffer();
-
-        short bytesRead = apdu.setIncomingAndReceive();
-        short echoOffset = (short) 0;
-
-        while (bytesRead > 0) {
-            Util.arrayCopyNonAtomic(buffer, ISO7816.OFFSET_CDATA, echoBytes, echoOffset, bytesRead);
-            echoOffset += bytesRead;
-            bytesRead = apdu.receiveBytes(ISO7816.OFFSET_CDATA);
-        }
-
-        apdu.setOutgoing();
-        apdu.setOutgoingLength((short) (echoOffset + 5));
-
-        // echo header
-        apdu.sendBytes((short) 0, (short) 5);
-        // echo data
-        apdu.sendBytesLong(echoBytes, (short) 0, echoOffset);
+        apdu.setIncomingAndReceive();
+        Util.arrayCopyNonAtomic(HELLO_WORLD, (short) 0, buffer, (short) 0, (short) HELLO_WORLD.length);
+        apdu.setOutgoingAndSend((short) 0, (short) HELLO_WORLD.length);
     }
 }
-//import javacard.framework.APDU;
-//import javacard.framework.Applet;
-////import javacard.framework.Util;
-//
-///**
-// * Hello World Applet.
-// */
-//public class HelloWorldApplet extends Applet {
-//
-////    private final static byte[] HELLO_WORLD = new byte[] {'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!'};
-//
-//    public HelloWorldApplet() {
-//        register();
-//    }
-//
-//    /**
-//     * Installs the applet.
-//     *
-//     * @param bArray
-//     *            array with installation parameters.
-//     * @param bOffset
-//     *            offset into array.
-//     * @param bLength
-//     *            the length of the parameters.
-//     */
-//    public static void install(byte[] bArray, short bOffset, byte bLength) {
-//        new HelloWorldApplet();
-//    }
-//
-//    /**
-//     * Processes an incoming APDU.
-//     *
-//     * @param apdu
-//     *            the APDU.
-//     */
-//    public void process(APDU apdu) {
-//        byte buffer[] = apdu.getBuffer();
-//        apdu.setIncomingAndReceive();
-////        Util.arrayCopyNonAtomic(HELLO_WORLD, (short) 0, buffer, (short)0, (short)HELLO_WORLD.length);
-////        apdu.setOutgoingAndSend((short)0, (short)HELLO_WORLD.length);
-//    }
-//
-//}
