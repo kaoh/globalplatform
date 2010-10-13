affee9fd77c2bdc695cee497225f42a22c8f29cd
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2010-10-13 15:42:00 +0000

    changed version number

--- trunk/gpshell/make.bat
+++ trunk/gpshell/make.bat
@@ -1,2 +1,2 @@
-set VERSION=2.0.0
+set VERSION=1.4.4
 nmake -f Makefile.mak %1 %2 SDK_LIB1="C:\Programme\Microsoft Platform SDK for Windows Server 2003 R2\Lib" SDK_LIB2="C:\Programme\Microsoft Visual Studio 9.0\VC\lib" SDK_LIB3="C:\WinDDK\7600.16385.1\lib\win7\i386" SDK_LIB4="C:\WinDDK\7600.16385.1\lib\Crt\i386"
