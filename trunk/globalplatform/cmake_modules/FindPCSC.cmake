e9b2dc2b9244b2876fb665c760f6ce8dc0d900da
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2012-11-11 19:42:06 +0000

    Merge from cmake Branch

---
+++ trunk/globalplatform/cmake_modules/FindPCSC.cmake
@@ -0,0 +1,31 @@
+# - Try to find the PC/SC smart card  library
+# Once done this will define
+#
+#  PCSC_FOUND - system has the PC/SC library
+#  PCSC_INCLUDE_DIRS - the PC/SC include directory
+#  PCSC_LIBRARIES - The libraries needed to use PC/SC
+#
+# Author: F. Kooman <fkooman@tuxed.net>
+# Version: 20101019
+#
+
+FIND_PACKAGE (PkgConfig)
+IF(PKG_CONFIG_FOUND)
+    # Will find PC/SC library on Linux/BSDs using PkgConfig
+    PKG_CHECK_MODULES(PCSC libpcsclite)
+#   PKG_CHECK_MODULES(PCSC QUIET libpcsclite)   # IF CMake >= 2.8.2?
+ENDIF(PKG_CONFIG_FOUND)
+
+IF(NOT PCSC_FOUND)
+   # Will find PC/SC headers both on Mac and Windows
+   FIND_PATH(PCSC_INCLUDE_DIRS NAMES WinSCard.h winscard.h)
+   # PCSC library is for Mac, WinSCard library is for Windows
+   FIND_LIBRARY(PCSC_LIBRARIES NAMES PCSC WinSCard winscard)
+ENDIF(NOT PCSC_FOUND)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(PCSC DEFAULT_MSG
+  PCSC_LIBRARIES
+  PCSC_INCLUDE_DIRS
+)
+MARK_AS_ADVANCED(PCSC_INCLUDE_DIRS PCSC_LIBRARIES)
