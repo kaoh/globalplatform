508a1a38dbb4ff9596439287ab5d222ee9569fb5
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2014-02-25 13:30:58 +0000

    included key derivation functions in public API

--- globalplatform/src/globalplatform/globalplatform.h
+++ globalplatform/src/globalplatform/globalplatform.h
@@ -876,6 +876,21 @@ OPGP_API
 OPGP_ERROR_STATUS OP201_VISA1_derive_keys(OPGP_CARD_CONTEXT cardContext, OPGP_CARD_INFO cardInfo, OP201_SECURITY_INFO *secInfo, BYTE masterKey[16],
 								 BYTE S_ENC[16], BYTE S_MAC[16], BYTE DEK[16]);
 
+//! \brief Derives the static keys from a master key according the VISA 2 key derivation scheme.
+OPGP_API
+OPGP_ERROR_STATUS VISA2_derive_keys(BYTE baseKeyDiversificationData[10], BYTE masterKey[16],
+							BYTE S_ENC[16], BYTE S_MAC[16], BYTE DEK[16]);
+
+//! \brief Derives the static keys from a master key according the VISA 1 key derivation scheme.
+OPGP_API
+OPGP_ERROR_STATUS VISA1_derive_keys(BYTE cardSerialNumber[8], BYTE masterKey[16],
+							BYTE S_ENC[16], BYTE S_MAC[16], BYTE DEK[16]);
+
+//! \brief Derives the static keys from a master key according the EMV CPS11 derivation scheme.
+OPGP_API
+OPGP_ERROR_STATUS EMV_CPS11_derive_keys(BYTE baseKeyDiversificationData[10], BYTE masterKey[16],
+							BYTE S_ENC[16], BYTE S_MAC[16], BYTE DEK[16]);
+
 #ifdef __cplusplus
 }
 #endif
