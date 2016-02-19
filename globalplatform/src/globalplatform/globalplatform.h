6e0d6525847260d13e95facdf5db68d318eb13cb
k_o_ <k_o_@4fd78bb6-c0b0-451e-b2ae-8295a1ca034b>
2016-02-19 15:37:40 +0000

    using UTF-8 encoding

--- globalplatform/src/globalplatform/globalplatform.h
+++ globalplatform/src/globalplatform/globalplatform.h
@@ -1,4 +1,4 @@
-﻿/*  Copyright (c) 2013, Karsten Ohme
+/*  Copyright (c) 2013, Karsten Ohme
  *  This file is part of GlobalPlatform.
  *
  *  GlobalPlatform is free software: you can redistribute it and/or modify
