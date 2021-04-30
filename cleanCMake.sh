92293aaa644bb8735dc2797d4225864bc196457b
Karsten Ohme <k_o_@users.sourceforge.net>
2021-04-30 02:41:30 +0200

    CMake version update

--- cleanCMake.sh
+++ cleanCMake.sh
@@ -1,12 +1,17 @@
-#!/bin/sh
-CMD="rm -f CMakeDoxy* && rm -f CTest* && rm -f install_manifest.txt && rm -f CMakeCache.txt && rm -f cmake_install.cmake && rm -rf CMakeFiles && rm -rf _CPack_Packages && rm -f CPack* && rm -rf src/CMakeFiles"
-$(${CMD})
+#!/bin/bash
+CMD1="rm -f CMakeDoxy* && rm -f CTest* && rm -f install_manifest.txt && rm -f CMakeCache.txt && rm -f cmake_install.cmake"
+CMD2="rm -rfd CMakeFiles && rm -rf _CPack_Packages && rm -f CPack* && rm -rfd src/CMakeFiles"
+$(${CMD1})
+$(${CMD2})
 cd gpshell 
-$(${CMD})
+$(${CMD1})
+$(${CMD2})
 cd ..
 cd globalplatform
-$(${CMD})
+$(${CMD1})
+$(${CMD2})
 cd ..
 cd gppcscconnectionplugin
-$(${CMD})
+$(${CMD1})
+$(${CMD2})
 cd ..
