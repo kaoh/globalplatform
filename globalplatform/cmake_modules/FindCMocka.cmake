8c468b573117d7d91326a0202db231931c123e64
Karsten Ohme <k_o_@users.sourceforge.net>
2022-04-03 17:22:51 +0200

    fixed cmocka windows library path

--- globalplatform/cmake_modules/FindCMocka.cmake
+++ globalplatform/cmake_modules/FindCMocka.cmake
@@ -3,7 +3,7 @@
 # Once done this will define
 #
 #  CMOCKA_INCLUDE_DIRS   - where to find cmocka.h, etc.
-#  CMOCKA_LIBRARIES      - List of libraries when using cmocka.
+#  CMOCKA_LINK_LIBRARIES - List of libraries when using cmocka.
 #  CMOCKA_FOUND          - True if cmocka found.
 #
 PKG_CHECK_MODULES(CMOCKA cmocka>=1.1)
@@ -27,17 +27,17 @@ set(CMOCKA_NAMES cmocka)
 
 foreach(search ${_CMOCKA_SEARCHES})
   find_path(CMOCKA_INCLUDE_DIR NAMES cmocka.h        ${${search}} PATH_SUFFIXES include)
-  find_library(CMOCKA_LIBRARY  NAMES ${CMOCKA_NAMES} ${${search}} PATH_SUFFIXES lib)
+  find_library(CMOCKA_LINK_LIBRARY  NAMES ${CMOCKA_NAMES} ${${search}} PATH_SUFFIXES lib)
 endforeach()
 
-mark_as_advanced(CMOCKA_LIBRARY CMOCKA_INCLUDE_DIR)
+mark_as_advanced(CMOCKA_LINK_LIBRARY CMOCKA_INCLUDE_DIR)
 
 include(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CMOCKA REQUIRED_VARS CMOCKA_LIBRARY CMOCKA_INCLUDE_DIR)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(CMOCKA REQUIRED_VARS CMOCKA_LINK_LIBRARY CMOCKA_INCLUDE_DIR)
 
 if(CMOCKA_FOUND)
     set(CMOCKA_INCLUDE_DIRS ${CMOCKA_INCLUDE_DIR})
-    set(CMOCKA_LIBRARIES ${CMOCKA_LIBRARY})
+    set(CMOCKA_LINK_LIBRARIES ${CMOCKA_LINK_LIBRARY})
 endif()
 
 ENDIF(NOT CMOCKA_FOUND)
