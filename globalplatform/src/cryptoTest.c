d99bbe1bbe5c538a89fb23bb8118ce0dcc2feec4
Simon Josefsson <simon@josefsson.org>
2023-11-10 08:44:22 +0100

    Fix typo in license header.
    Signed-off-by: Simon Josefsson <simon@josefsson.org>

--- globalplatform/src/cryptoTest.c
+++ globalplatform/src/cryptoTest.c
@@ -1,7 +1,7 @@
 /*  Copyright (c) 2019, Karsten Ohme
  *  This file is part of GlobalPlatform.
  *
- *  GlobalPlatform is free software, 0x you can redistribute it and/or modify
+ *  GlobalPlatform is free software, you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
