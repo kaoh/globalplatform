36a8e60a42acf063f1f467f897707a4bebc17c9b
Karsten Ohme <k_o_@users.sourceforge.net>
2026-02-17 23:14:45 +0100

    Add WiX patch for PATH environment variable and improve PCSC detection logic
    - Introduce a WiX patch to configure PATH environment variable updates during installation.
    - Refactor `FindPCSC.cmake` to improve platform-specific library detection, prioritizing the WinSCard library on Windows.

--- globalplatform/cmake_modules/FindPCSC.cmake
+++ globalplatform/cmake_modules/FindPCSC.cmake
@@ -9,20 +9,26 @@
 # Version: 20101019
 #
 
-IF(UNIX)
-  FIND_PACKAGE (PkgConfig)
+IF(WIN32)
+  # On Windows, always use the OS-provided WinSCard import library from the
+  # active toolchain/SDK. This avoids accidentally picking third-party PC/SC
+  # libraries from unrelated environments on PATH/LIB.
+  FIND_PATH(PCSC_INCLUDE_DIRS NAMES WinSCard.h winscard.h)
+  set(PCSC_LIBRARIES WinSCard)
+ELSE()
+  FIND_PACKAGE(PkgConfig)
   IF(PKG_CONFIG_FOUND)
       # Will find PC/SC library on Linux/BSDs using PkgConfig
       PKG_CHECK_MODULES(PCSC libpcsclite)
-  ENDIF(PKG_CONFIG_FOUND)
-ENDIF()
+  ENDIF()
 
-IF(NOT PCSC_FOUND)
-   # Will find PC/SC headers both on Mac and Windows
-   FIND_PATH(PCSC_INCLUDE_DIRS NAMES WinSCard.h winscard.h)
-   # PCSC library is for Mac, WinSCard library is for Windows
-   FIND_LIBRARY(PCSC_LIBRARIES NAMES PCSC WinSCard winscard)
-ENDIF(NOT PCSC_FOUND)
+  IF(NOT PCSC_FOUND)
+     # Will find PC/SC headers both on Mac and Unix
+     FIND_PATH(PCSC_INCLUDE_DIRS NAMES WinSCard.h winscard.h)
+     # PCSC library is for Mac and other Unix-like systems
+     FIND_LIBRARY(PCSC_LIBRARIES NAMES PCSC)
+  ENDIF()
+ENDIF()
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(PCSC DEFAULT_MSG
